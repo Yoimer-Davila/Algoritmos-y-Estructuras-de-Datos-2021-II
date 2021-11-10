3cb7ff918c701d470fa5e763da39895c64382fb7
YoimerDavila <89022117+YoimerDavila@users.noreply.github.com>
2021-11-10 18:47:39 -0500

    Update tree.cpp

--- Albol binario/tree.cpp
+++ Albol binario/tree.cpp
@@ -2,22 +2,31 @@
 #include "trees.hpp"
 
 //.cpp para las pruebas del encabezado trees.hpp
-
 int main() {
-	const int size = 5;
+	const int size = 10;
 	srand(time(nullptr));
-	BinaryTree<int*> tree;
+	//los mayores van a la izquierda --> [](int*& i, int*& j) {return *i < *j; }
+	BinaryTree<int*> tree([](int*& i, int*& j) {return *i < *j; });
+	for (size_t i = 0; i < size; ++i)
+		tree.insert(new int(10 - i));
 
-	int* aux = nullptr;
-	for (size_t i = 0; i < size; ++i) {
-		aux = new int(rand() % 1000);
-		tree.insert(aux);
-	}
-	cout << endl;
-	cout << "Height: " << tree.height() << endl;
-	cout << "Size: " << tree.size() << endl;
-
+	tree.post_order([](int*& i) {cout << *i << " "; });
 	
+	cout << endl;
+	
+	//devolvera el mayor valor, puesto que min devuelve el valor mas a la izquierda.
+	//Debido a que el criterio de comparacion envió a los mayores a la izquierda
+	cout << "Min: " << *tree.max() << endl;
+
+	//devolvera el menor valor, puesto que max devuelve el valor mas a la derecha.
+	//Debido a que el criterio de comparacion envió a los menores a la derecha
+	cout << "Max: " << *tree.min() << endl;
+	
+	cout << tree.find(new int(10)) << endl;
+
+	tree.in_order([](int*& i) {cout << *i << " "; });
+
+
 	cout << endl;
 	system("pause");
 	return 0;
