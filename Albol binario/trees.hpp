a4226bad040c561f18cc00510cf301ffb9576041
YoimerDavila <89022117+YoimerDavila@users.noreply.github.com>
2021-11-10 08:29:11 -0500

    Update trees.hpp

--- Albol binario/trees.hpp
+++ Albol binario/trees.hpp
@@ -25,9 +25,9 @@ struct Compare<T*> {
     //true if T is a pointer, false if not
     static const bool is_pointer = true;
     //return first parameter >= second parameter
-    static bool greater_equal(T*& i,T*& j) { return *i >= *j; }
+    static bool higher_equal(T*& i,T*& j) { return *i >= *j; }
     //return first parameter <= second parameter
-    static bool less_equal( T*& i, T*& j) { return *i <= *j; }
+    static bool less_equal(T*& i, T*& j) { return *i <= *j; }
     //return first parameter > second parameter
     static bool higher(T*& i, T*& j) { return *i > *j; };
     //return first parameter < second parameter
@@ -98,9 +98,42 @@ private:
         _function(node->value);
     }
 
+
+    //_function --> for erase value in the tree
+    //_s_function --> for left way, else rigth way
     template<typename BoolFunction>
-    void _erase(Node*& node, const T& value, BoolFunction _function) {
-        if (node == nullptr) return;
+    bool _erase(Node*& node, T& value, BoolFunction _function, BoolFunction _s_function) {
+        if (node == nullptr) return false;
+        
+        if (_function(node->value, value)) {
+            if (node->left == nullptr && node->rigth == nullptr) {
+                node = nullptr;
+                delete node;
+                return true;
+            }
+            else if (node->left == nullptr) {
+                node = node->rigth;
+                return true;
+            }
+            else if (node->rigth == nullptr) {
+                node = node->left;
+                return true;
+            }
+            else {
+                auto aux = node->rigth;
+                while (aux->left != nullptr)
+                    aux = aux->left;
+
+                node->value = aux->value;
+
+                return this->_erase(node->rigth, aux->value, _function, _s_function);
+            }
+        }
+        else {
+            if (_s_function(node->value, value))
+                return this->_erase(node->left, value, _function, _s_function);
+            else return this->_erase(node->rigth, value, _function, _s_function);
+        }
     }
 
     template<typename BoolFunction>
@@ -114,6 +147,20 @@ private:
         else return this->_find(node->rigth, value, _function, _s_function);
     }
 
+
+    T& _min(Node*& node) {
+        if (node->left == nullptr)
+            return node->value;
+        
+        return _min(node->left);
+    }
+
+    T& _max(Node*& node) {
+        if (node->rigth == nullptr)
+            return node->value;
+
+        return _max(node->rigth);
+    }
 
 public:
     BinaryTree() : root(nullptr), _size(0), _height(0), a_height(0) {}
@@ -153,14 +200,25 @@ public:
     template<typename Function>
     void post_order(Function _function) { this->_post_order(this->root, _function); }
 
-    //erase the value in the BinaryTree if exist, aun por desarrollar
-    void erase(T value) { this->_erase(this->root, value, Compare<T>::higher); }
+    //erase the value in the BinaryTree if exist
+    bool erase(T value) { return this->_erase(this->root, value, Compare<T>::equals, Compare<T>::higher); }
 
-    //erase the value in the BinaryTree if BoolFunction return true, aun por desarrollar
+    //erase the value in the BinaryTree if exist
+    //BoolFunction es una funcion booleana que recibe dos elementos de tipo T
+    //_f_equals is the criteria that will be used to remove the value if it returns true
+    //_function is the criterion to go left if it returns true
     template<typename BoolFunction>
-    void erase(T value, BoolFunction _function) { this->_erase(this->root, value, _function); }
+    bool erase(T value, BoolFunction _f_equals, BoolFunction _function) { this->_erase(this->root, value, _f_equals, _function); }
 
     bool find(T value) { return this->_find(this->root, value, Compare<T>::equals, Compare<T>::higher); }
+
+    //returns the minimum element --> returns the value most to the left of the tree
+    //if your criteria inserted the minors to the right, use max() as min()
+    T& min() { return this->_min(this->root); }
+
+    //returns the maximum element -> returns the value most to the right of the tree
+    //if your criteria inserted the highers on the left, use min() as max()
+    T& max() { return this->_max(this->root); }
 
     //return the number of elements in the BinaryTree
     ull size() const { return this->_size; }
